c308f32c0dea7d129410cdd9d625a630d83e5759
Kretz Mathis <mathis.kretz@swisscom.com>
2017-03-11 23:07:54 +0100

    add redux

--- app/index.jsx
+++ app/index.jsx
@@ -4,17 +4,34 @@ import './fonts/index.css';
 import {Router, Route, browserHistory, IndexRedirect} from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import Notifications from './notifications/notifications.jsx';
+import {routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
+import reducer from './reducer.jsx';
 
 injectTapEventPlugin();
 
+const routerMiddlewareBrowserHistory = routerMiddleware(browserHistory);
+
+let store =  createStore(
+    reducer,
+    applyMiddleware(
+        routerMiddlewareBrowserHistory
+    )
+);
+
+const history = syncHistoryWithStore(browserHistory, store)
+
 let routes = (
-  <MuiThemeProvider>
-    <Router history={browserHistory}>
-      <Route path="/" component={Notifications}>
-      </Route>
-    </Router>
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <Router history={browserHistory}>
+        <Route path="/" component={Notifications}>
+        </Route>
+      </Router>
+    </MuiThemeProvider>
+  </Provider>
 );
 
 render(routes, document.getElementById('app'));
