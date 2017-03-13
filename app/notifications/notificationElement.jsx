b727befb8574393382472b5daeb9d261ace3fdaf
Kretz Mathis <mathis.kretz@swisscom.com>
2017-03-13 08:55:53 +0100

    add delete button on notifications

--- app/notifications/notificationElement.jsx
+++ app/notifications/notificationElement.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import {ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import ActionGrade from 'material-ui/svg-icons/action/grade';
+import IconButton from 'material-ui/IconButton';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
 
 const NotificationElement = ({notification}) => (
-  <div key={notification.id}>
-    <ListItem primaryText={notification.text} leftIcon={<ActionGrade />} /> <Divider />
+  <div>
+    <ListItem
+      primaryText={notification.text}
+      leftIcon={<ActionGrade />}
+      rightIconButton={<IconButton tooltip="dismiss"><ActionDelete /></IconButton>}/>
+    <Divider />
   </div>
 )
 
